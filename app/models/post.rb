535c942db5f69a9f8f98a2f6c7bd61b9f39738a7
Ori Markowitz <ori.markowitz@gmail.com>
2020-12-14 17:30:00 -0500

    commit tree 848c8fd90accf757446e0099363878e98f11d3dc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ActiveRecord::Base
+    validate :title_must_be_clickbait_y
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top [0-9]*/i,
+        /Guess/i
+    ]
+    
+    def title_must_be_clickbait_y
+        # if !self.title.include?(["Won't Believe",
+        #     "Secret", "Top", "Guess"])
+        #     error.add(:title, "Not clickbait-y enough!")
+        # end
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    # include ActiveModel::Validations
+    # validates_with TitleValidator
 end
